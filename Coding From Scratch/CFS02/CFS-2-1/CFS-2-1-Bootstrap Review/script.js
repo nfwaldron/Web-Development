0a2814859487eb6b8cff9344b0e0dfc3c1c72c9e
Nathan Waldron <waldron.nathan@gmail.com>
2015-06-01 15:37:49 -0400

    Update

---
+++ Coding From Scratch/CFS02/CFS-2-1/CFS-2-1-Bootstrap Review/script.js
@@ -0,0 +1,9 @@
+// Used to pass information to the user
+alert("Nathan Waldron");
+
+//Print information to the developer
+console.log("Print Message");
+
+console.log(document.getElementById("heading"));
+
+document.getElementById("heading").innerHTML="Hello From Javascript";
