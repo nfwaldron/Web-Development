9dfa5bc9a6fb1b5608fb564cbfac6bb1a077c42a
Nathan Waldron <waldron.nathan@gmail.com>
2015-06-10 19:41:10 -0400

    update

--- Coding From Scratch/CFS03/CFS-3-3/CFS-3-3-Javascript CRUD/script.js
+++ Coding From Scratch/CFS03/CFS-3-3/CFS-3-3-Javascript CRUD/script.js
@@ -1,80 +1,81 @@
-var garage=[];
-
-// create a variable to store ID
+var garage = [];
 var newId = 0;
+var selectedCarId;
 
-function car(make, year, image)
-{
-	this.id;
-	this.make = make;
-	this.year = year;
-	this.image = image;
+
+function car(make, year, image) {
+    this.id;
+    this.make = make;
+    this.year = year;
+    this.image = image;
+};
+
+function addCar() {
+    var make = document.getElementById("carMake").value;
+    var year = document.getElementById("carYear").value;
+    var image = document.getElementById("carImg").value;
+    var carObj = new car(make, year, image);
+    carObj.id = newId;
+    newId++;
+    garage.push(carObj);
+    console.log(garage);
+    showCars();
+};
+
+
+function editCar(id) {
+    for(var car in garage)
+    {
+        if (garage[car].id == id) {
+            document.getElementById("editMake").value = garage[car].make;
+            document.getElementById("editYear").value = garage[car].year;
+            document.getElementById("editImg").value = garage[car].image;
+            selectedCarId = id;
+        }
+    }
 }
 
-//Create
-
-function addCar(){
-	var make = document.getElementById('carMake').value;
-	var year = document.getElementById('carYear').value;
-	var image = document.getElementById('carImg').value;
-	var carObj = new car(make,year,image);
-
-	// assign the value of the number variable newId to the 
-	carObj.id = newId;
-	newId++;
-
-	garage.push(carObj);
-	console.log(garage);
-
-	// for the post to be displayed, you MUST include the showCar() function in the addCar() function!
-	showCar();
+function saveCar() {
+    for(var car in garage)
+    {
+        if (garage[car].id == selectedCarId) {
+            garage[car].make = document.getElementById("editMake").value;
+            garage[car].year = document.getElementById("editYear").value;
+            garage[car].image = document.getElementById("editImg").value;
+        }
+    }
+    showCars();
 }
 
-function editCar(id)
-{
-	for (var car in garage)
-	{
-		if (garage[car].id === id)
-		{	
-			document.getElementById("editMake").value=garage[car].make;
-			document.getElementById("editYear").value=garage[car].year;
-			document.getElementById("editImg").value=garage[car].image;
-		}
-	}
+function deleteCar(id) {
+    var index;
+    for (var i = 0; i < garage.length; i++) {
+        if (garage[i].id == id) {
+            index = i;
+            break;
+        }
+    }
+    garage.splice(index, 1);
 
+    showCars();
 }
+//  [ car0, car1, car2, car3, car4 ]
+// [ { id = 0, make = "honda", year = 2001, image = ""}, { id = 1 make = "bmw", year = 2013, image = ""}, { id = 2, make = "VW", year = 1989, image = ""} ]
+function showCars() {
+    document.getElementById("carDisplay").innerHTML = "";
+    var h = "";
+    for (var car in garage) {
+        h += "<h1>" + garage[car].make + "</h1>"
+        + "<p>" + garage[car].year + "</p>"
+        + "<img width='250' src='" + garage[car].image + "' /><br />"
+        + "<button onclick='editCar(" + garage[car].id + ")'>Edit</button>"
+        + "<button onclick='deleteCar(" + garage[car].id + ")'>Delete</button>";
+        //<button onclick="deleteCar(some id)">Delete</button>
+    }
+    document.getElementById("carDisplay").innerHTML = h;
 
-function saveCar(id)
-{
-	for (var car in garage)
-	{		
-		if (garage[car].id === id)
-	{	
-		garage[car].make = document.getElementById("editMake").value;
-		garage[car].year = document.getElementById("editYear").value;
-		garage[car].image = document.getElementById("editImg").value;
-	}
+    document.getElementById("carMake").value = "";
+    document.getElementById("carYear").value = "";
+    document.getElementById("carImg").value = "";
+};
 
-	}
-
-	showCar();
-
-}
-
-function showCar()
-{
-	document.getElementById('carDisplay').innerHTML = "";
-
-	for(var car in garage)
-	{
-		document.getElementById('carDisplay').innerHTML += "<h1>" + garage[car].make 
-		+ "</h1><p>" + garage[car].year + "</p> <img  width = '250' src ='" + 
-		garage[car].image + "'/><br>" + 
-		 "<button onclick='editCar(" + garage[car].id + ")'>Edit</button>"
-		 + "<button onclick='saveCar( " + garage[car].id + ")'>Save</button>" ;
-	}
-
-document.getElementById("carMake").value = "";
-document.getElementById("carYear").value = "";
-document.getElementById("carImg").value = "";
-}
